54bb955198b62aa8f5ae3f38837822bd16fac66f
millernerd <millernerd@gmail.com>
2021-11-20 20:05:09 -0700

    added spacing

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+
 console.log("Hello World");
+
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
     // res.send("Hello Express")
   res.sendFile(__dirname + '/views/index.html')
